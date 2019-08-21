aa2baeb64de15f0abfba50782ecb39d8d3288666
charlescortez <charlesmph@gmail.com>
2019-08-21 16:11:21 -0400

    Added content to register page, linked login (empty) and register page to homepage, added head + footer on every page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'MeetDesserts' });
+    res.render('index', { title: 'MeetDesserts' });
+});
+
+router.get('/register', (req, res, next)=>{
+    res.render('register', {
+
+  });
+});
+
+router.get('/login', (req, res, next)=>{
+    res.render('login', {
+
+  });
 });
 
 module.exports = router;

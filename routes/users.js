61af15aabf83336872404430bb9c9a39997a3d3a
Sean Yim <superjjang89@gmail.com>
2019-08-22 10:34:01 -0400

    added login route, finished login template/look/yeah

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../db')
+var bcrypt = require('bcrypt')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+console.log('hi')
+router.post('/loginProcess', function(req, res){
+  const checkUserQuery = `
+  SELECT * FROM users WHERE users = $1
+  `;
+  db.one(checkUserQuery, [req.body.username]).then( resp =>{
+    // const correctPass = bcrypt.compareSync(req.body.password, resp.password);
+    res.json(resp)
+  })
+  .catch((err)=>{
+    res.json(err);
+  })
+})
+
+
+
+
 
 module.exports = router;
